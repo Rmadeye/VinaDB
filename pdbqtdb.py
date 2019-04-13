21fe2c2c97d12b017e815f5b97cffc5ec17c845f
Rmadeye <46814304+Rmadeye@users.noreply.github.com>
2019-04-13 10:19:39 +0200

    Improved table naming

--- pdbqtdb.py
+++ pdbqtdb.py
@@ -6,11 +6,11 @@ def df2sqlite(dataframe,tbl_name,db_name="Vina_database.db"):
     cur = conn.cursor()
 
     wildcards = ','.join(['?'] * len(dataframe.columns))
-    tbl_name = [tuple(x) for x in dataframe.values]
+    data = [tuple(x) for x in dataframe.values]
 
     col_str = '"' + '","'.join(dataframe.columns) + '"'
     cur.execute("create table IF NOT EXISTS %s (%s)" % (tbl_name, col_str))
-    cur.executemany("insert into %s values(%s)" % (tbl_name, wildcards), tbl_name)
+    cur.executemany("insert into %s values(%s)" % (tbl_name, wildcards), data)
 
     cur.execute("DELETE FROM tbl_name WHERE rowid NOT IN (SELECT min(rowid) FROM tbl_name GROUP BY Filename,RMSDBestMode)")
 
